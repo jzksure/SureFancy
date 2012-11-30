4634d91d34ed8a0d5467fb08cb234e1f341f8dfe
Jessica Schurr <jessicaschurr@gmail.com>
2012-11-30 11:56:29 -0700

    contact, thank you, motion,
    contact form, thank you page, motion graphics and video page

--- server.rb
+++ server.rb
@@ -1,4 +1,15 @@
 require 'sinatra'
+require 'pony'
+
+post '/contact' do
+  message = params[:message]
+  name = message[:name]
+  email = message[:email]
+  body = params[:body]
+
+  Pony.mail(:to => 'jessica@maru-creative.com', :from => email, :subject => 'Website inquiry from' + name, :body => body)
+  File.read(File.join('public', 'contact-thanks.html'))
+end
 
 get '/' do
   File.read(File.join('public', 'index.html'))
