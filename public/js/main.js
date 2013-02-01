7a2bddf052d039898842c92eae9b51c10f1fa5b4
Jessica Schurr <jessicaschurr@gmail.com>
2013-02-01 15:54:03 -0700

    crispin changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 
   /* automatically cycle through the pages */
-  var timer = setInterval(function() {
+  /*var timer = setInterval(function() {
     currentPage = $(".sel_page:checked");
     currentPage.next().click();
-  }, 6000);
+  }, 6000);*/
   
   /* cancel automatic rotation if the user clicks an arrow */
-  $(".pss_contr").click(function() {
+  /*$(".pss_contr").click(function() {
     clearInterval(timer);
   })
 
-});
+});*/
